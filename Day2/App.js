4863b07457ed0415e173a709ad6509bc049e45dc
Sudhanshu <Sudhanshu17cr7@gmail.com>
2023-03-15 23:21:05 +0530

    test

--- Day2/App.js
+++
@@ -1,5 +0,0 @@
-const heading1  = React.createElement("h1", {id:"heading" , juju:"zuzu", key:"1"}, "Hum bhi bana lenge")
-const heading2  = React.createElement("h2", {id:"heading2" , juju:"tina", key:"2"}, "Tum bhi bana loge")
-const container = React.createElement("div",{id:"container"}, [heading1,heading2] )
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(container)
