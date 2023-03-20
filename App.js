2183887f6872c279adc4859fee9b7f1536fdf7df
Sudhanshoo <Sudhanshu17cr7@gmail.com>
2023-03-20 10:34:01 +0530

    anugrah4

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 
 
 const heading1  = React.createElement("h1", {id:"heading" , juju:"zuzu", key:"1"}, "H bana lenge")
-const heading2  = React.createElement("h2", {id:"heading2" , juju:"tina", key:"2"}, "Tum bhi bana loge")
+const heading2  = React.createElement("h2", {id:"heading2" , juju:"tina", key:"2"}, " bhi bana loge")
 const container = React.createElement("div",{id:"container"}, [heading1,heading2] )
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(container)
